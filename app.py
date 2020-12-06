7022f1b3a4739ce61779dd7c3845b0f27cc0d0a1
Daksh Chordiya <dakshchordiya007@gmail.com>
2020-12-06 13:00:53 +0530

    changed setup.sh

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import numpy as np
 
 
 from sklearn.svm import SVC
+
 from sklearn.linear_model import LogisticRegression
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier  
 from sklearn.svm import LinearSVC
--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
-mkdir -p ~/.streamlit/                                              
-echo "\                       
-[server]\n\                       
-port = $PORT\n\                       
-enableCORS = false\n\                       
-headless = true\n\                       
-\n\                       
-" > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/
+echo “\
+[general]\n\
+email = \"dakshchordiya007@gmail.com\"\n\
+“ > ~/.streamlit/credentials.toml
+echo “\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+“ > ~/.streamlit/config.toml
